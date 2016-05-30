859bd0bf64ef44ebbed0912b0f507624c0f93328
Stefan Breunig <stefan-github@yrden.de>
2016-05-30 18:34:21 +0200

    maybe a bit higher quality

--- runner.sh
+++ runner.sh
@@ -5,7 +5,7 @@ cd $(dirname $(readlink -f $0))
 filename="$(date +%F__%H:%M:%S).png"
 
 wget -q -O"${filename}" "http://pattern.zmaw.de/fileadmin/user_upload/pattern/radar/lawr_4.png"
-pngquant --ext .png --force -Q 40 "${filename}"&
+pngquant --ext .png --force -Q 60 "${filename}"&
 find *.png -mmin +120 -exec rm {} \;
 
 images=($(ls *.png))
