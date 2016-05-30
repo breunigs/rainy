69531d8f9f29191210d740d83bc3ad3fcde8f89b
Stefan Breunig <stefan-github@yrden.de>
2016-05-30 18:18:35 +0200

    optimize PNGs to save a bit more bandwidth

--- runner.sh
+++ runner.sh
@@ -5,6 +5,7 @@ cd $(dirname $(readlink -f $0))
 filename="$(date +%F__%H:%M:%S).png"
 
 wget -q -O"${filename}" "http://pattern.zmaw.de/fileadmin/user_upload/pattern/radar/lawr_4.png"
+optipng -quiet -o7 "${filename}"&
 find *.png -mmin +60 -exec rm {} \;
 
 images=($(ls *.png))
